ed63e509008eaaa372737e61d02adc1ae8acdb62
ljinjie <74753622+ljinjie@users.noreply.github.com>
2021-12-07 23:22:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import numpy as np
 import argparse
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch-size', type=int, default=4, help='batch-size fo training, default=4')
-parser.add_argument('--epochs', type=int, default=1000, help='number of training epochs, default=1000')
-parser.add_argument('--learning-rate', type=float, default=0.000001, help='learning rate, default=0.001')
+parser.add_argument('--batch-size', type=int, default=12, help='batch-size fo training, default=4')
+parser.add_argument('--epochs', type=int, default=400, help='number of training epochs, default=500')
+parser.add_argument('--learning-rate', type=float, default=0.001, help='learning rate, default=0.001')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum, default=0.9')
 parser.add_argument('--use-cpu', action='store_true', default=False, help='use CPU only for training, default=False')
 parser.add_argument('--in-channel', type=int, default=3, help='input channels, default=3')
-parser.add_argument('--out-channel', type=int, default=32, help='output channels, default=32')
+parser.add_argument('--out-channel', type=int, default=31, help='output channels, default=31')
 
 params = parser.parse_args()
 device = torch.device("cuda" if torch.cuda.is_available() and (not params.use_cpu) else "cpu")
@@ -26,7 +26,7 @@ def train_epoch(train_loader, model, criterion, optimizer):
         Train the model for one iteration through the training set.
     """
     loss_total = 0.0
-    idx = 1
+    # idx = 1
     for idx, (X, y) in enumerate(train_loader, 1):
         # clear parameter gradients
         X = X.to(device)
@@ -48,13 +48,17 @@ def train_epoch(train_loader, model, criterion, optimizer):
 
 if __name__ == '__main__':
     classes = np.load('classes.npy')
-    x_train = KITTI(classes)
-    x_train_loader = torch.utils.data.DataLoader(x_train, batch_size=params.batch_size, shuffle=True, num_workers=4)
+    # ignore void label, class #30
+    classes_ignore = np.delete(classes, 30, axis=0)
+    x_train = KITTI(classes_ignore)
+    x_test = KITTI(classes_ignore, data_path='data/test', label_path='label/test')
+    x_train_loader = torch.utils.data.DataLoader(x_train, batch_size=params.batch_size, shuffle=True, num_workers=2)
+    x_test_loader = torch.utils.data.DataLoader(x_test, batch_size=params.batch_size, shuffle=True, num_workers=2)
 
     # training
-    model = SegNet().to(device)
+    model = SegNet(params.in_channel, params.out_channel).to(device)
     optimizer = optim.SGD(model.parameters(), lr=params.learning_rate, momentum=params.momentum)
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss(ignore_index=-1)
 
     model, start_epoch, stats = checkpoint.restore_checkpoint(model, 'checkpoints', cuda=True, force=False)
 
@@ -70,3 +74,35 @@ if __name__ == '__main__':
     print('-------------------')
     print('Training completed.')
     print('-------------------')
+
+    plt.semilogy(range(len(stats)), stats)
+    plt.title('Training Loss')
+    plt.show()
+
+    # testing
+    model.eval()
+    with torch.no_grad():
+        for i, (X, y) in enumerate(x_test_loader, 1):
+            X = X.to(device)
+            y = y.to(device)
+            y_pred = model(X)
+            prediction = torch.argmax(y_pred, dim=1).detach().cpu().numpy()
+            ground_truth = torch.argmax(y, dim=1).detach().cpu().numpy()
+
+            for j in range(prediction.shape[0]):
+                img_pred = np.zeros((prediction.shape[1], prediction.shape[2], 3))
+                img_true = np.zeros((prediction.shape[1], prediction.shape[2], 3))
+                for m in range(classes_ignore.shape[0]):
+                    mask_pred = (prediction[j] == m)
+                    mask_true = (ground_truth[j] == m)
+                    img_pred[mask_pred] = classes_ignore[m]
+                    img_true[mask_true] = classes_ignore[m]
+                img_pred /= 255.0
+                img_true /= 255.0
+                plt.imshow(img_pred)
+                plt.show()
+                plt.imshow(img_true)
+                plt.show()
+
+            if i >= 3:
+                break
